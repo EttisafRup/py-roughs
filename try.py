bb8b1be2a56ce1a2aae204f883e564d4e3e8d1f9
Ettisaf Rup <107711625+EttisafRup@users.noreply.github.com>
2022-11-30 16:31:59 +0000

    adasd

---
+++ try.py
@@ -0,0 +1,27 @@
+import os
+os.system("clear")
+##
+
+simple = "Rup is a very nice little boy!";
+simpleArr = ["Rup", "Suchishmeeta", "Anika", "Tisha"]
+simpleArrNum = [5,6,8,3,0,4,2]
+
+simpleArr.insert(1, 69)
+simpleArr.remove("Anika")
+print(simpleArr)
+
+tp = (1,2,3)
+print(tp[2])
+while True:
+    for i in simpleArr:
+        print(i)
+
+
+
+a = 10
+b = 20
+third_party = a # => 10
+a = b # => 20
+b = third_party # => 10
+
+print(a, b)
