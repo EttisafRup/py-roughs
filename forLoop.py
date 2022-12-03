1f197f0f8bc0b4242f1398a3347e26070ec46987
Ettisaf Rup <107711625+EttisafRup@users.noreply.github.com>
2022-12-03 15:26:20 +0000

    .

---
+++ forLoop.py
@@ -0,0 +1,5 @@
+array = [2, 3, "Mein", 5, 6, 88, "69", "asdas", 989, "dasdas"]
+
+for i in array:
+    if str(i).isnumeric():
+        print(i)
---
+++ whileLoop.py
@@ -0,0 +1,5 @@
+i = 0
+
+while i < 100:
+    print("I love you", str(i+1) + "+", "times Ammu 🖤")
+    i = i + 1
